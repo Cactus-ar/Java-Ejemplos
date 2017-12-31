d813d7ebb5205e06afdb9525400334b063cfb9f7
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2017-12-31 05:28:16 -0300

    primeros ejemplos
    primeros ejemplos del libro

---
+++ guide/1-13/Example.java
@@ -0,0 +1,13 @@
+/*
+Simple programa en Java
+Renombre este archivo como Example.java
+*/
+
+class Example {
+    //Un programa en Java comienza con una llamada a main()
+    public static void main (String args[]) {
+        System.out.println("Java maneja la web.");
+        
+    }
+    
+}
---
+++ guide/readme.txt
@@ -0,0 +1,9 @@
+En esta carpeta se encuentran todos los ejemplos
+que contiene el libro A Beginner´s Guide (Java)
+de Herbert Schildt
+
+Los números de las carpetas siguen el patrón
+de Capítulo-Página (Ejemplo: 1-17) de la séptima
+edición del libro.
+
+
