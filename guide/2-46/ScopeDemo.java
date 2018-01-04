77aaf8c103164e0db4e5f271698ac23c34a79d11
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2018-01-04 05:13:43 -0300

    capitulo 2 cont

---
+++ guide/2-46/ScopeDemo.java
@@ -0,0 +1,27 @@
+/**
+ * Demostrar la visibilidad de las variables
+ * 
+ * ScopeDemo.java
+ */
+class ScopeDemo {
+    public static void main(String args[]) {
+        int x; //es visible dentro de todo el bloque de codigo main
+
+        x = 10;
+
+        if(x == 10){ //un nuevo bloque de codigo
+            int y = 20; //solamente visible dentro del bloque
+
+            // x e y son visibles aqui
+            System.out.println("x e y: " + x + " " + y);
+            x = y * 2;
+        }
+
+        // y = 100; //Error! y no es conocido en este ambito
+        // x si lo es
+        System.out.println("x es " + x);
+        
+    }
+
+    
+}
---
+++ guide/2-48/ModDemo.java
@@ -0,0 +1,25 @@
+/**
+ * Demuestra el uso del operador %
+ * ModDemo.java
+ */
+class ModDemo {
+    public static void main(String args[]) {
+        int iresult, irem;
+        double dresult, drem;
+
+        iresult = 10 / 3;
+        irem = 10 % 3;
+
+        dresult = 10.0 / 3.0;
+        drem = 10.0 % 3.0;
+
+        System.out.println("Resultado y resto de 10/3: " + iresult + " " + irem);
+
+        System.out.println("Resultado y resto de 10.0/3.0: " + dresult + " " + drem);
+
+        
+    }
+
+
+    
+}
